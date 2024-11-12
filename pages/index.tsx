bcfe74003dc0d2adef1a5c8695c96a66274be4c5
vaibhavd2103 <bountydange@gmail.com>
2024-11-12 13:47:46 +0100

    patch: download button align

--- pages/index.tsx
+++ pages/index.tsx
@@ -113,7 +113,7 @@ export default function Home() {
               <p>No data found.</p>
             </div>
           ) : (
-            <div className="w-full h-full px-0 flex- flex-col justify-center items-center">
+            <div className="w-full h-full px-0 flex flex-col justify-center items-center">
               <PlotlyChart data={weatherData} />
               <button
                 className="rounded-lg bg-gray-800 text-white px-4 py-2 mt-4 self-center"
